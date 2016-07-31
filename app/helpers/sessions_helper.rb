819532334475a64c0399ffeeb3dcba407cc9b2e2
ndbiller <a.biller@doxter.de>
2016-08-01 00:08:47 +0200

    update user implemented 1/2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
