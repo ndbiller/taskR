1b0e899c9be6b66c617ad8bfb7cd2977df7f05c8
ndbiller <a.biller@doxter.de>
2016-07-31 22:52:51 +0200

    implement basic user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get  '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
+  root   'static_pages#home'
+  get    'static_pages/home'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
